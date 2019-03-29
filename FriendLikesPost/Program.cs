1d2623a18c4657ff9100f1a1aa2ca62efc49e590
Debashis <jupiter_104@ymail.com>
2019-03-29 18:28:06 +0530

    Add files via upload

---
+++ FriendLikesPost/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FriendLikesPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var friendNames = new List<string>();
+            while (true)
+            {
+                Console.WriteLine("Type your friends' names. Hit Enter after each name.");
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+                friendNames.Add(input);
+            }
+
+            int numberOfFriends = friendNames.Count;
+            if (numberOfFriends == 0)
+            {
+                return;
+            }
+            else if (numberOfFriends == 1)
+            {
+                Console.WriteLine(friendNames.First() + " likes your post");
+                Console.ReadLine();
+            }
+            else if (numberOfFriends == 2)
+            {
+                Console.WriteLine("{0} and {1} like your post", friendNames.First(), friendNames.ElementAt(1));
+                Console.ReadLine();
+            }
+            else if (numberOfFriends >= 3)
+            {
+                Console.WriteLine("{0}, {1} and {2} other(s) like your post", friendNames.First(), friendNames.ElementAt(1), numberOfFriends - 2);
+                Console.ReadLine();
+            }           
+        }
+    }
+}
